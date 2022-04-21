681f29eb867c7e96a4eb40e32fc2e567d49bd996
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-21 08:21:09 +0000

    Refactored Publisher Code - Modified code to register new Publisher

---
+++ handlers/publishers/publisher.js
@@ -0,0 +1,37 @@
+import * as filesystem from 'fs';
+import { nanoid } from "nanoid";
+import { JSONWriterGeneric } from '../utilities/jsonWriter.js';
+import { validatePublisher } from "./validate.js";
+
+const PublisherRegistryFilePath = "logfiles/PublisherRegistry.json";
+
+export function getPublishersRegistry() {
+    const PublisherRegistry = JSON.parse(
+        filesystem.readFileSync(PublisherRegistryFilePath)
+    );
+    return PublisherRegistry;
+}
+
+export function addToPublishersRegistry(newPublisher) {
+    const PublisherRegistry = getPublishersRegistry();
+    console.log(PublisherRegistry)
+    if (validatePublisher(newPublisher, PublisherRegistry)) {
+        const publisherNamespacePath = "logfiles/" + newPublisher["publisher"];
+        filesystem.mkdir(publisherNamespacePath, function (err) {
+            if (err) throw err;
+            const newPublisherProfile = {
+                "id": nanoid(8),
+                "timestamp": Date.now(),
+                "description": newPublisher["description"],
+                "path": publisherNamespacePath
+            }
+            PublisherRegistry[newPublisher["publisher"]] = newPublisherProfile;
+            JSONWriterGeneric(PublisherRegistryFilePath, PublisherRegistry, function (err) {
+                console.log(err);
+                return "success";
+            })
+        })
+    }else{
+        return "userAlreadyExist"
+    }
+}
