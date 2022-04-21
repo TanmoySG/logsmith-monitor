56cb551e215b0f8c9892703fbae1a9af25758ffb
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-21 20:10:49 +0000

    Updated App.js to reflect Other code changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { addToPublisherRegistry } from "./handlers/publishers/publisher.js";
+import { addToPublisherRegistry, getPublishersRegistry } from "./handlers/publishers/publisher.js";
+import { validateExistingPublisher } from "./handlers/publishers/validate.js";
 
 const app = express();
 const PORT = 8080;
@@ -9,36 +10,41 @@ app.use(express.json());
 app.get("/", function (request, response) {
     response.send({
         message: "Welcome to Logsmith Monitor!",
-        isValid: JSON.stringify(addToPublisherRegistry({
-            "publisher" : "tester",
-            "description" : "Fucl"
-        }))
+        version: "logsmith-monitor v0.1.0"
     })
 })
 
-
+// Documentation Route - for new Publisher
 app.get("/publisher/new", function (request, response) {
     response.send({
         greetings: "Welcome to Logsmith Monitor Documentation!",
         message: "Use this route in POST method to register new publisher.",
-        route : "{url}:{port}/publisher/new",
-        method : "POST",
+        route: "{url}:{port}/publisher/new",
+        method: "POST",
         body: {
-            publisher : "A Recognizable and Short Name of the Publisher, Eg. App1",
-            origin : "Origin of the Publisher - Technical Name or URI. Eg. backend.app1.com:5000",
-            description : "Description of the Publisher. Eg. The Backend of App1"
+            publisher: "A Recognizable and Short Name of the Publisher, Eg. App1",
+            origin: "Origin of the Publisher - Technical Name or URI. Eg. backend.app1.com:5000",
+            description: "Description of the Publisher. Eg. The Backend of App1"
         }
     })
 })
 
-
+// Usage Route - for new Publisher
 app.post("/publisher/new", function (request, response) {
-    // const newPublisherRequestProfile = request.body;
-    addToPublisherRegistry(request.body, function(PublisherRegistryResponse){
-        response.status(PublisherRegistryResponse['status'] == "success" ? 200 : 412 );
+    const newPublisherRequestProfile = request.body;
+    addToPublisherRegistry(newPublisherRequestProfile, function (PublisherRegistryResponse) {
+        response.status(PublisherRegistryResponse['status'] == "success" ? 200 : 412);
         response.send(PublisherRegistryResponse);
     });
 })
+
+
+app.get("/:publisher/context/new", function (request, response) {
+    const Publisher = request.params["publisher"];
+    const PublisherRegistry = getPublishersRegistry();
+
+})
+
 
 app.get("/publish/logs", function (request, response) {
     response.send({
@@ -47,10 +53,11 @@ app.get("/publish/logs", function (request, response) {
             context: "Name of Component/App Pushing the Log",
             log: { "doc": "Put a JSON Object with all the logging details" },
             status: "INFO | WARN | SUCCESS | FAILURE | CRITICAL",
-            subcontext : "sub.contexts.to.a.context"
+            subcontext: "sub.contexts.to.a.context"
         }
     })
 })
+
 
 app.post("/publish/logs", function (request, response) {
     response.send({
@@ -60,7 +67,7 @@ app.post("/publish/logs", function (request, response) {
             log: { "doc": "Put a JSON Object with all the logging details" },
             status: "INFO | WARN | SUCCESS | FAILURE | CRITICAL",
             message: "Overview of the Log",
-            subcontext : "subcontexts.to.a.context"
+            subcontext: "subcontexts.to.a.context"
         }
     })
 })
