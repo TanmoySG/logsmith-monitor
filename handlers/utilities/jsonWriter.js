838c4fc6d60c13376eb3ada06a1e30e99bb3f6e5
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-21 07:07:58 +0000

    Added Utilities Directory to put all Generically usable Code - added jsonWriter Utility

---
+++ handlers/utilities/jsonWriter.js
@@ -0,0 +1,12 @@
+import * as filesystem from 'fs';
+
+export function JSONWriterGeneric(filePath, contentToWrite, callback) {
+    const jsonifiedContentToWrite = JSON.stringify(contentToWrite, null, 4);
+    filesystem.writeFile(filePath, jsonifiedContentToWrite, function (err) {
+        if (err) {
+            callback(err);
+        }else{
+            callback(true);
+        }
+    })
+}
