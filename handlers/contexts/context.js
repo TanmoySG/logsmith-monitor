4c709cc327278000049a27b2ae541afbf074a20f
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-22 06:57:24 +0000

    Updated ContextRegistry Code #4

--- handlers/contexts/context.js
+++ handlers/contexts/context.js
@@ -1,4 +1,6 @@
+import * as filesystem from 'fs';
 import { JSONWriterGeneric } from "../utilities/jsonWriter.js";
+import { validateExistingPublisher } from '../publishers/validate.js';
 
 const ContextRegistryTemplate = {}
 
@@ -6,4 +8,25 @@ export function createContextRegistry(publisherNamespacePath, callback) {
     JSONWriterGeneric(publisherNamespacePath + "/ContextRegistry.json", ContextRegistryTemplate, function (err) {
         callback(err);
     });
+}
+
+export function getContextRegistry(publisher) {
+    const ContextRegistryFilePath = `logfiles/${publisher}/ContextRegistry.json`;
+    const ContextRegistry = JSON.parse(
+        filesystem.readFileSync(ContextRegistryFilePath)
+    );
+    return ContextRegistry;
+}
+
+export function addToContextrRegistry(publisher, newContext, callback) {
+    const ContextRegistry = getContextRegistry(publisher);
+    if(validateExistingPublisher(publisher)){
+        
+    }else{
+       callback({
+           status : "failed",
+           message : "Publisher not in Registry."
+       })
+    }
+    
 }
---
+++ handlers/contexts/validate.js
@@ -0,0 +1,15 @@
+export function validateNewContext(newContext, ContextRegistry) {
+    if (newContext["context"] in ContextRegistry) {
+        return false
+    } else {
+        if ("publisher" in newPublisher && "origin" in newPublisher) {
+            if (newPublisher["publisher"].length != 0 && newPublisher["origin"].length != 0) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+}
