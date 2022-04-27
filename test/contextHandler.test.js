bc33a7116807b0f7a2f52875dc032c5445e56085
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-27 17:34:37 +0000

    Added Tests for all cases - validateNewContext() #9

--- test/contextHandler.test.js
+++ test/contextHandler.test.js
@@ -16,27 +16,59 @@ const validNewContextData = {
             "logs": ["column1", "column2"],
             "metrics": ["column1"]
         }
-    },
-    validNewContextTwo: {
-        "context": "context#4",
-        "origin": "test.context.handler:1000/context#4",
-        "description": "Test Context - Test #4",
+    }
+}
+
+const invalidNewContextData = {
+    invalidNewContextOne: {
+        "context": "context#1",
+        "origin": "test.context.handler:1000/context#1",
+        "description": "Test Context - Test #1",
         "kind": {
             "logs": [],
             "metrics": []
         }
+    },
+    invalidNewContextTwo : {
+        "context": "context#3",
+        "origin": "test.context.handler:1000/context#3",
+        "description": "Test Context - Test #3",
+        "kind": {}
+    },
+    invalidNewContextThree : {
+        "context": "context#3",
+        "description": "Test Context - Test #3",
+        "kind": {}
+    },
+    invalidNewContextThree : {
+        "context": "context#3",
+        "origin": "",
+        "description": "Test Context - Test #3",
+        "kind": {}
     }
-}
-
-const invalidContextData = {
-
 }
 
 // ContextHandler - Validation Check - Tests
 describe("ContextHandler-Validate Tests", function(){
-    describe("validateNewContext() Test", function(){
-        it("Should Return True", function(){
+    describe("Testing Method - validateNewContext()", function(){
+        it("Test Case #1 [ Success Case - Valid Context        ] [ Should Return True  ]", function(){
             expect(validateNewContext(validNewContextData.validNewContextOne, TestContextRegistry)).to.equal(true)
+        })
+
+        it("Test Case #2 [ Failure Case - Context Exists       ] [ Should Return False ]", function(){
+            expect(validateNewContext(invalidNewContextData.invalidNewContextOne, TestContextRegistry)).to.equal(false)
+        })
+
+        it("Test Case #3 [ Failure Case - Kind Value Missing   ] [ Should Return False ]", function(){
+            expect(validateNewContext(invalidNewContextData.invalidNewContextTwo, TestContextRegistry)).to.equal(false)
+        })
+
+        it("Test Case #3 [ Failure Case - Origin Missing       ] [ Should Return False ]", function(){
+            expect(validateNewContext(invalidNewContextData.invalidNewContextThree, TestContextRegistry)).to.equal(false)
+        })
+
+        it("Test Case #3 [ Failure Case - Invalid Origin Value ] [ Should Return False ]", function(){
+            expect(validateNewContext(invalidNewContextData.invalidNewContextThree, TestContextRegistry)).to.equal(false)
         })
     })
 })
