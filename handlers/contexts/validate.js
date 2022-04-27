e615854e042d838a50d39a3353d2af75e04c77ca
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-27 06:47:27 +0000

    Updated NewContext Validity Code

--- handlers/contexts/validate.js
+++ handlers/contexts/validate.js
@@ -1,16 +1,12 @@
 
 export function validateNewContext(newContext, ContextRegistry) {
-    // Check if Context Already exists in ContextRegistry
     if (newContext["context"] in ContextRegistry) {
         return false
     } else {
-        // Check if ContextName, Origin and Kind are present in NewContext 
         if ("context" in newContext && "origin" in newContext && "kind" in newContext) {
-            // Check for ContextName and Origin Validity
             if (newContext["context"].length < 0 && newContext["origin"].length < 0) {
                 return false
             }
-            // Cheeck if either of logs or metrics are present
             if (!("logs" in newContext["kind"]) || !("metrics" in newContext["kind"])) {
                 return false
             }
