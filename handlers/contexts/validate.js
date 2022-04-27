80bb9fa61630f308857e9caeaa2515c8f9fd1ff5
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-27 06:44:18 +0000

    Added NewContext Validity Code

--- handlers/contexts/validate.js
+++ handlers/contexts/validate.js
@@ -1,17 +1,26 @@
-// import 
 
 export function validateNewContext(newContext, ContextRegistry) {
+    // Check if Context Already exists in ContextRegistry
     if (newContext["context"] in ContextRegistry) {
         return false
     } else {
-        if ("publisher" in newPublisher && "origin" in newPublisher) {
-            if (newPublisher["publisher"].length != 0 && newPublisher["origin"].length != 0) {
-                return true
-            } else {
+        // Check if ContextName, Origin and Kind are present in NewContext 
+        if ("context" in newContext && "origin" in newContext && "kind" in newContext) {
+            // Check for ContextName and Origin Validity
+            if (newContext["context"].length < 0 && newContext["origin"].length < 0) {
                 return false
             }
+            // Cheeck if either of logs or metrics are present
+            if (!("logs" in newContext["kind"]) || !("metrics" in newContext["kind"])) {
+                return false
+            }
+            return true
         } else {
             return false
         }
     }
+}
+
+export function hasSchema(newContext, ContextRegistry){
+
 }
