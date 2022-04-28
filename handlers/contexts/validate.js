b297be1fd0aa853aeb4616832ca05c44acce154c
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-28 06:42:54 +0000

    Added validateLogHasSchema() and validateMetricHasSchema() to check if Schema Exists

--- handlers/contexts/validate.js
+++ handlers/contexts/validate.js
@@ -24,6 +24,30 @@ export function validateNewContext(newContext, ContextRegistry) {
     }
 }
 
-export function hasSchema(newContext, ContextRegistry) {
+export function validateLogHasSchema(newContext) {
+    if (Array.isArray(newContext["kind"]["logs"])) {
+        if (newContext["kind"]["logs"].length > 0) {
+            if (newContext["kind"]["logs"].every(columnName => ["timestamp", "scope"].includes(columnName))) {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+}
 
+export function validateMetricHasSchema(newContext) {
+    if (Array.isArray(newContext["kind"]["metrics"])) {
+        if (newContext["kind"]["metrics"].length > 0) {
+            if (newContext["kind"]["metrics"].every(key => ["timestamp"].includes(key))) {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
 }
