4ea419f45b376851150e157aad7f508abd749e03
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-02 06:34:48 +0000

    Updated Log Handler Code - Added Methods #6

--- handlers/logsHandler/logs.js
+++ handlers/logsHandler/logs.js
@@ -1,4 +1,12 @@
 import { JSONWriterGeneric } from '../utilities/jsonWriter.js';
+import { validateExistingPublisher } from '../publishers/validate.js'
+import { getPublisherRegistry } from '../publishers/publisher'
+import { getContextRegistry } from '../contexts/context.js';
+import { validateExistingContext } from '../contexts/validate.js';
+
+export function usesSchema(LogRegistry) {
+    return LogRegistry.usesSchema
+}
 
 export function createLogRegistry(contextNamespacePath, logTemplate, callback) {
     const logNamespace = contextNamespacePath + "/" + "LogRegistry.json"
@@ -7,3 +15,25 @@ export function createLogRegistry(contextNamespacePath, logTemplate, callback) {
     })
 }
 
+export function getLogRegistry(publisher, context) {
+    const LogRegistryFilePath = `logfiles/${publisher}/${context}/LogRegistry.json`;
+    const LogRegistry = JSON.parse(
+        filesystem.readFileSync(LogRegistryFilePath)
+    );
+    return LogRegistry;
+}
+
+export function registerNewLog(publisher, context, newLog, callback) {
+    const PublisherRegistry = getPublisherRegistry();
+    if (validateExistingPublisher(publisher, PublisherRegistry)){
+        const ContextRegistry = getContextRegistry(publisher);
+        if(validateExistingContext(context, ContextRegistry)){
+            const LogRegistry = getLogRegistry(publisher, context);
+            if(usesSchema(LogRegistry)){
+
+            }else{
+                
+            }
+        }
+    }
+}
