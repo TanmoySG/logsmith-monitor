e9b68c74aaa9bab81977e3de98e7687a2b3c44db
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-02 19:38:36 +0000

    Manual Testing Changes

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -1,15 +1,49 @@
-import { addToContextsRegistry } from "./contexts/context.js";
-
+import { registerNewLog } from "./logsHandler/logs.js";
 const publisher = "tester";
-const newContext = {
-    "context" : "test-context",
-    "origin" : "app.something.com:1000/context1",
-    "description" : "Lorem Ipsum",
-    "kind" : {
-        "logs" : ["col1","col2"],
-    }  
+const context = "test-context";
+// const newContext = {
+//     "context" : "test-context",
+//     "origin" : "app.something.com:1000/context1",
+//     "description" : "Lorem Ipsum",
+//     "kind" : {
+//         "logs" : ["col1","col2"],
+//     }  
+// }
+
+// addToContextsRegistry(publisher, newContext, function(ret){
+//     console.log(ret)
+// })
+
+const logg1 = {
+    "col1" : "test",
+    "col2": "test2",
+    "timestamp" : 123456,
+    "status" : "WARN"
 }
 
-addToContextsRegistry(publisher, newContext, function(ret){
-    console.log(ret)
-})
+const logg2={
+    "col1" : "test",
+    "col2": "test2",
+    "status" : "WARN"
+}
+
+const logg3={
+    "col" : "test",
+    "col2": "test2",
+    "status" : "WARN"
+}
+
+registerNewLog(publisher, context,logg1, function (err) {
+    console.log(err)
+    registerNewLog(publisher, context,logg2, function (err) {
+        console.log(err)
+        registerNewLog(publisher, context,logg3, function (err) {
+            console.log(err)
+        })
+    });
+});
+
+
+
+
+
