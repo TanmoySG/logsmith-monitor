8e7004b881d1b8c73fb946de662e597c6e7e02bc
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-08 20:25:07 +0000

    Updated logHandler to use standardized Identifiers #10 #8

--- handlers/logsHandler/logs.js
+++ handlers/logsHandler/logs.js
@@ -5,6 +5,7 @@ import { getPublishersRegistry } from '../publishers/publisher.js'
 import { getContextRegistry } from '../contexts/context.js';
 import { validateExistingContext } from '../contexts/validate.js';
 import { validateSchema } from './validate.js';
+import { StandardizeIdentifier } from '../utilities/identifierUtility.js';
 
 export function usesSchema(LogRegistry) {
     return LogRegistry.usesSchema
@@ -26,6 +27,8 @@ export function getLogRegistry(publisher, context) {
 }
 
 export function registerNewLog(publisher, context, newLog, callback) {
+    publisher = StandardizeIdentifier(publisher)
+    context = StandardizeIdentifier(context)
     const PublisherRegistry = getPublishersRegistry();
     if (validateExistingPublisher(publisher, PublisherRegistry)) {
         const ContextRegistry = getContextRegistry(publisher);
@@ -69,6 +72,8 @@ export function registerNewLog(publisher, context, newLog, callback) {
 }
 
 export function getLogs(publisher, context, callback) {
+    publisher = StandardizeIdentifier(publisher)
+    context = StandardizeIdentifier(context)
     const PublisherRegistry = getPublishersRegistry();
     if (validateExistingPublisher(publisher, PublisherRegistry)) {
         const ContextRegistry = getContextRegistry(publisher);
