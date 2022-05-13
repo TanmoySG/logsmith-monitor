73c0efd24d3ec1fa17c22b56c5f350cab761d767
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-13 20:30:09 +0000

    Updated Example and lib code

---
+++ libraries/js/examples/index.js
@@ -0,0 +1,4 @@
+import Logsmith from "../logsmith/logsmith.js";
+
+const logg = new Logsmith({})
+console.log(logg.getParams())
--- libraries/js/logsmith/logsmith.js
+++ libraries/js/logsmith/logsmith.js
@@ -1,13 +1,13 @@
 import chalk from 'chalk';
 
-const LISTENER
+// const LISTENER
 
 const logLevels = {
     WARN: chalk.yellow,
     INFO: chalk.blue,
-    CRITICAL : chalk.bgRed.gray,
-    SUCCESS : chalk.green,
-    FAILURE : chalk.red
+    CRITICAL: chalk.bgRed.gray,
+    SUCCESS: chalk.green,
+    FAILURE: chalk.red
 }
 
 // console.log(logLevels.INFO("INFO"))
@@ -16,8 +16,14 @@ const logLevels = {
 // console.log(logLevels.SUCCESS("SUCCESS"))
 // console.log(logLevels.FAILURE("FAILURE"))
 
-class Logsmith{
-    constructor(){
-        
+export default class Logsmith {
+    constructor(options) {
+        this.env = options.env || "default"
+        this.logfile = options.logfile || null
+        this.console_only = options.console_only || true
+    }
+
+    getParams() {
+        return this.env
     }
 }
