6aeada563ebf5ff0ce7093dddd847e60984afbc6
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-14 08:07:18 +0000

    Added String Templating #39

--- libraries/js/examples/index.js
+++ libraries/js/examples/index.js
@@ -1,4 +1,4 @@
-import Logsmith from "logsmith";
+import Logsmith from "../logsmith/logsmith.js";
 
 const logg = new Logsmith({})
-console.log(logg.getParams())
+console.log(logg.log({ level: "INFO", body: { col1: "val1", col2: "val2" } }))
