2a20950e8fb1fca48eb6f5193d7d49bca0b111d3
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-15 20:34:46 +0000

    Updated Example

--- libraries/js/examples/index.js
+++ libraries/js/examples/index.js
@@ -1,10 +1,7 @@
 import Logsmith from "../logsmith/logsmith.js";
 
 const logg = new Logsmith({})
-console.log(logg)
 
-logg.fetchConfigFromFile("/workspaces/logsmith-monitor/libraries/js/examples/configs/jsonConfig.json")
 
-console.log(logg)
+logg.WARN({ name: "tanmoy" })
 
-// console.log(logg.log({ level: "INFO", body: { col1: "val1", col2: "val2" } }))
