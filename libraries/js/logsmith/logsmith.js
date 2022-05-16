82478ab165e84a7abc3f0154d489de424718f26a
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-16 19:42:00 +0000

    Updated Code to add other LogLevel Methods #39

--- libraries/js/logsmith/logsmith.js
+++ libraries/js/logsmith/logsmith.js
@@ -6,12 +6,12 @@ import { readConfigFile } from './lib/fetchConfigs.js';
 import { JSONLogDriver } from './lib/drivers.js';
 import { consoleLogJSON, prepareJSONLog } from './lib/logUtility.js';
 
+const defaultLogPrintPattern = "[{level}] {body}"
+
 const LogFormats = {
     JSON: "json",
     STATEMENT: "statement"
 }
-
-const defaultLogPrintPattern = "[{level}] {body}"
 
 const ChalkLog = {
     WARN: chalk.yellowBright,
@@ -55,28 +55,45 @@ export default class Logsmith {
         }
     }
 
-    WARN(log) {
+    INFO(log) {
         if (this.logFormat == LogFormats.JSON) {
-            prepareJSONLog(LogLevels.WARN, log, this.env, function (JSONLog) {
-                consoleLogJSON(ChalkLog.WARN, JSONLog)
+            prepareJSONLog(LogLevels.INFO, log, this.env, function (JSONLog) {
+                consoleLogJSON(LogLevels.INFO, ChalkLog.INFO, JSONLog)
             })
         }
     }
 
-    INFO(log) {
-
+    WARN(log) {
+        if (this.logFormat == LogFormats.JSON) {
+            prepareJSONLog(LogLevels.WARN, log, this.env, function (JSONLog) {
+                consoleLogJSON(LogLevels.WARN, ChalkLog.WARN, JSONLog)
+            })
+        }
     }
 
-    CRITICAL(log) {
 
+    CRITICAL(log) {
+        if (this.logFormat == LogFormats.JSON) {
+            prepareJSONLog(LogLevels.CRITICAL, log, this.env, function (JSONLog) {
+                consoleLogJSON(LogLevels.CRITICAL, ChalkLog.CRITICAL, JSONLog)
+            })
+        }
     }
 
     SUCCESS(log) {
-
+        if (this.logFormat == LogFormats.JSON) {
+            prepareJSONLog(LogLevels.SUCCESS, log, this.env, function (JSONLog) {
+                consoleLogJSON(LogLevels.SUCCESS, ChalkLog.SUCCESS, JSONLog)
+            })
+        }
     }
 
     FAILURE(log) {
-
+        if (this.logFormat == LogFormats.JSON) {
+            prepareJSONLog(LogLevels.FAILURE, log, this.env, function (JSONLog) {
+                consoleLogJSON(LogLevels.FAILURE, ChalkLog.FAILURE, JSONLog)
+            })
+        }
     }
 
 }
