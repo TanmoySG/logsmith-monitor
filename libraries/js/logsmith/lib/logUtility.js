d0fee8bee30ef05f0da2eda4858de594444ad135
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-16 06:42:35 +0000

    Updated log.WARN Code

--- libraries/js/logsmith/lib/logUtility.js
+++ libraries/js/logsmith/lib/logUtility.js
@@ -1,19 +1,21 @@
+import chalk from 'chalk';
 import format from 'string-template';
 
-export function prepareJSONLog(logLevel, log, env) {
+
+export function prepareJSONLog(logLevel, log, env, callback) {
     const JSONLog = {
         timestamp: Date.now(),
         logLevel: logLevel,
         env: env,
         ...log
     }
-    return JSONLog
+    callback(JSONLog)
 }
 
 export function consoleLogJSON(chalkMode, JSONLog){
     console.log(
         chalkMode(`[${JSONLog.logLevel}]`),
-        JSONLog
+        JSON.stringify(JSONLog)
     )
 }
 
