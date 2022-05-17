1fed57b9b5f0a5bccf02c55a8e8864c85ff22040
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-17 06:52:22 +0000

    Updated Example

--- libraries/js/examples/index.js
+++ libraries/js/examples/index.js
@@ -5,14 +5,15 @@ const logg = new Logsmith({})
 
 logg.WARN({ name: "tanmoy" })
 
-logg.INFO({ name: "tanmoy" })
+// logg.INFO({ name: "tanmoy" })
 
-logg.SUCCESS({ name: "tanmoy" })
+// logg.SUCCESS({ name: "tanmoy" })
 
 logg.fetchConfigFromFile("./libraries/js/examples/configs/jsonConfig.json")
+logg.INFO({ name: "tanmoy" })
 
-logg.FAILURE({ name: "tanmoy" })
+// logg.FAILURE({ name: "tanmoy" })
 
-logg.CRITICAL({ name: "tanmoy" })
+// logg.CRITICAL({ name: "tanmoy" })
 
-logg.LOG("tRiaLalge", { name: "Tanmoy" })
+// logg.LOG("TRAILING", { name: "Tanmoy" })
