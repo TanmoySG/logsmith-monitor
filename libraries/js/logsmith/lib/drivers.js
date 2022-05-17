799c56784da6bc28d19c04cfa71635aa774ab608
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-17 20:09:32 +0000

    Added loggerRunner driver method to handle logging flow

--- libraries/js/logsmith/lib/drivers.js
+++ libraries/js/logsmith/lib/drivers.js
@@ -1,7 +1,15 @@
-import { prepareJSONLog } from "./logUtility.js"
+import { ChalkLog, LogFormats } from './specs.js';
+import { prepareJSONLog, consoleLogJSON, prepareStatementLog, consoleLogStatement } from './logUtility.js';
 
-export function JSONLogDriver(log, loglevel, logenv, callback) {
-    const preparedJSONLog = prepareJSONLog(loglevel, log, logenv)
-    
-    callback(preparedJSONLog)
+export function loggerRunner(logLevel, env, compiledLogPattern, logFormat, log) {
+    const chalkLog = ChalkLog[logLevel] || ChalkLog.CUSTOM
+    if (logFormat == LogFormats.JSON) {
+        prepareJSONLog(logLevel, log, env, function (LogJSON) {
+            consoleLogJSON(logLevel, chalkLog, LogJSON)
+        })
+    } else if (logFormat == LogFormats.STATEMENT) {
+        prepareStatementLog(logLevel, log, env, compiledLogPattern, function (LogStatement) {
+            consoleLogStatement(logLevel, chalkLog, LogStatement)
+        })
+    }
 }
