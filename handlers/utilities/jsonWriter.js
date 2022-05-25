02d526a975195748f3483688c66be40592944866
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-25 20:05:54 +0000

    Using writeFileSync inplace of writeFile in JSONWriterGeneric

--- handlers/utilities/jsonWriter.js
+++ handlers/utilities/jsonWriter.js
@@ -2,11 +2,6 @@ import * as filesystem from 'fs';
 
 export function JSONWriterGeneric(filePath, contentToWrite, callback) {
     const jsonifiedContentToWrite = JSON.stringify(contentToWrite, null, 4);
-    filesystem.writeFile(filePath, jsonifiedContentToWrite, function (err) {
-        if (err) {
-            callback(err);
-        }else{
-            callback();
-        }
-    })
+    filesystem.writeFileSync(filePath, jsonifiedContentToWrite)
+    callback()
 }
