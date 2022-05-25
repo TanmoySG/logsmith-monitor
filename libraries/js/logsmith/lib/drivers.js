4a26a8ed84ebe22c07d9918785935ba3a2ff5c58
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-05-25 20:10:17 +0000

    Updated Driver Code

--- libraries/js/logsmith/lib/drivers.js
+++ libraries/js/logsmith/lib/drivers.js
@@ -1,6 +1,7 @@
 import { consoleLogJSON, consoleLogStatement, prepareJSONLog, prepareStatementLog } from './logUtility.js';
 import { ChalkLog, LogFormats } from './specs.js';
 import { writeLogToFile } from './logFileUtility.js';
+import { monitorLogRunner } from '../monitor/monitor.js';
 
 function transformStatementToJSON(statement) {
     const transformedLog = {
@@ -10,18 +11,25 @@ function transformStatementToJSON(statement) {
 }
 
 
-export function loggerRunner(logLevel, env, compiledLogPattern, logFormat, consoleOnly, logFilePath, log) {
+export function loggerRunner(logLevel, env, compiledLogPattern, logFormat, consoleOnly, logFilePath, monitorLogging, monitorConfig, log) {
     if (typeof log == "string") {
         log = transformStatementToJSON(log)
     }
+    const LISTENER = monitorConfig.monitorListener
     const chalkLog = ChalkLog[logLevel] || ChalkLog.CUSTOM
     if (logFormat == LogFormats.JSON) {
         prepareJSONLog(logLevel, log, env, function (LogJSON) {
+            if (monitorLogging) {
+                monitorLogRunner(LISTENER, monitorConfig, LogJSON, function (response) { return response })
+            }
             writeLogToFile(LogJSON, logFilePath, consoleOnly)
             consoleLogJSON(logLevel, chalkLog, LogJSON)
         })
     } else if (logFormat == LogFormats.STATEMENT) {
         prepareStatementLog(logLevel, log, env, compiledLogPattern, function (LogStatement, LogJSON) {
+            if (monitorLogging) {
+                monitorLogRunner(LISTENER, monitorConfig, LogJSON, function (response) { return response })
+            }
             writeLogToFile(LogJSON, logFilePath, consoleOnly)
             consoleLogStatement(logLevel, chalkLog, LogStatement)
         })
