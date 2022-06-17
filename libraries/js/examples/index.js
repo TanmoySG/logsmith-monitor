621e5c6c4ed77628846fc67c515339bf160a78a3
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Updated Example"
    This reverts commit 49ac850627e9778f2f7264e620d06355acc492a9.

--- libraries/js/examples/index.js
+++ libraries/js/examples/index.js
@@ -5,12 +5,3 @@ const logg = new Logsmith({})
 
 logg.WARN({ name: "tanmoy" })
 
-logg.INFO({ name: "tanmoy" })
-
-logg.SUCCESS({ name: "tanmoy" })
-
-logg.fetchConfigFromFile("./libraries/js/examples/configs/jsonConfig.json")
-
-logg.FAILURE({ name: "tanmoy" })
-
-logg.CRITICAL({ name: "tanmoy" })
