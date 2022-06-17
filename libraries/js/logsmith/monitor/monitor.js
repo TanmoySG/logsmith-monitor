fdf7af456862fba695a76ef07bc61ed645dd2ab2
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Added Test Logging to Monitor"
    This reverts commit bb15995d7e841df5c8148dfbdc6f98856be06bfd.

--- libraries/js/logsmith/monitor/monitor.js
+++ libraries/js/logsmith/monitor/monitor.js
@@ -8,27 +8,14 @@ const Endpoints = {
     Log: compile("{0}/{1}/{2}/logs")
 }
 
-function prepareRequestConfig(method, body) {
-    return {
-        method: method,
-        body: JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' }
-    }
-}
 
 export function logToMonitor(listener, publisher, context, log, callback) {
-    const logURI = Endpoints.Log(listener, publisher, context)
-    fetch(logURI, prepareRequestConfig("POST", log)).then(function (response) {
+    const logURI = Endpoints.API(listener)
+    fetch(logURI).then(function (response) {
         return response.json()
     }).then(function (response) {
         console.log(response)
     })
 }
 
-logToMonitor("http://localhost:8080", "testapp001", "testcontext001", {
-    "logLevel": "INFO",
-    "message": "Testing Logsmith",
-    "mode": "RESTClient"
-})
-
-// console.log(prepareRequestConfig("POST", { "test": "pass" }))
+logToMonitor("http://localhost:8080")
