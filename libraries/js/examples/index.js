d30b440c3b916d6bd2c67cd50473765976ec182d
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Updated Example"
    This reverts commit ef86ecd66f8f94dc9ded9ae0b9450830f07260c3.

--- libraries/js/examples/index.js
+++ libraries/js/examples/index.js
@@ -10,58 +10,59 @@ const logg = new Logsmith({})
 logg.fetchConfigFromFile(configFilePath)
 logg.initializeMonitor()
 
-const SampleValidLogExt = {
-    "source": "example",
-    "issue": "none"
-}
-
 app.use(express.json());
 
 app.get("/warn", function (request, response) {
-    logg.WARN({ "trial": "W", ...SampleValidLogExt })
+    logg.WARN({ "trial": "W" })
     response.send({
-        "log": "sent",
-        "level": "warn"
+        message: "Welcome to Logsmith Example!",
+        version: "logsmith-monitor v0.1.0-alpha - Example",
+        level: "warn"
     })
 })
 
 app.get("/info", function (request, response) {
-    logg.INFO({ ...SampleValidLogExt })
+    logg.INFO({ "trial": "I" })
     response.send({
-        "log": "sent",
-        "level": "info"
+        message: "Welcome to Logsmith Example!",
+        version: "logsmith-monitor v0.1.0-alpha - Example",
+        level: "info"
     })
 })
 
 app.get("/critical", function (request, response) {
     logg.CRITICAL({ "trial": "C" })
     response.send({
-        "log": "sent",
-        "level": "critical"
+        message: "Welcome to Logsmith Example!",
+        version: "logsmith-monitor v0.1.0-alpha - Example",
+        level: "critical"
     })
 })
 
 app.get("/success", function (request, response) {
     logg.SUCCESS({ "trial": "S" })
     response.send({
-        "log": "sent",
-        "level": "success"
+        message: "Welcome to Logsmith Example!",
+        version: "logsmith-monitor v0.1.0-alpha - Example",
+        level: "success"
     })
 })
 
 app.get("/failure", function (request, response) {
     logg.FAILURE({ "trial": "F" })
     response.send({
-        "log": "sent",
-        "level": "failure"
+        message: "Welcome to Logsmith Example!",
+        version: "logsmith-monitor v0.1.0-alpha - Example",
+        level: "failure"
     })
 })
 
 app.get("/log", function (request, response) {
     logg.LOG("TRAILING...", { "trial": "L" })
     response.send({
-        "log": "sent",
-        "level": "log"
+        message: "Welcome to Logsmith Example!",
+        version: "logsmith-monitor v0.1.0-alpha - Example",
+        level: "log"
     })
 })
 
