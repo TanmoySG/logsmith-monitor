d9c026a6e57c84335eb8dc051af92a4bf3971775
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Added Initial WARN Log Support"
    This reverts commit b86f01d4a9f5ef5e0a2370c4e10a475404e631ab.

--- libraries/js/logsmith/lib/logUtility.js
+++ libraries/js/logsmith/lib/logUtility.js
@@ -1,23 +1,10 @@
 import format from 'string-template';
 
-export function prepareJSONLog(logLevel, log, env) {
-    const JSONLog = {
-        timestamp: Date.now(),
-        logLevel: logLevel,
-        env: env,
-        ...log
-    }
-    return JSONLog
+export function prepareLogJSON(logLevel, log) {
+    
 }
 
-export function consoleLogJSON(chalkMode, JSONLog){
-    console.log(
-        chalkMode(`[${JSONLog.logLevel}]`),
-        JSONLog
-    )
-}
+export function prepareLogStatement(logLevel, log, logFormat) {
 
-export function prepareStatementLog(logLevel, log, logFormat) {
-
-
+    
 }
