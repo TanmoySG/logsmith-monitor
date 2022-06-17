c5f5016e1c88a88125517f7a547b69c291eec36a
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Added JSONLog Driver Code"
    This reverts commit 1f734435fab1f5110be15cc86b643c1498f06cdc.

--- libraries/js/logsmith/lib/drivers.js
+++
@@ -1,7 +0,0 @@
-import { prepareJSONLog } from "./logUtility.js"
-
-export function JSONLogDriver(log, loglevel, logenv, callback) {
-    const preparedJSONLog = prepareJSONLog(loglevel, log, logenv)
-    
-    callback(preparedJSONLog)
-}
