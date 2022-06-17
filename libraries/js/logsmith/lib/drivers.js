bb52bd2f33fd19a2d0bc856f0c488df5695a95b1
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Added loggerRunner driver method to handle logging flow"
    This reverts commit c6a5321d7bbef173d73b7aa9650315791fb87382.

--- libraries/js/logsmith/lib/drivers.js
+++ libraries/js/logsmith/lib/drivers.js
@@ -1,15 +1,7 @@
-import { ChalkLog, LogFormats } from './specs.js';
-import { prepareJSONLog, consoleLogJSON, prepareStatementLog, consoleLogStatement } from './logUtility.js';
+import { prepareJSONLog } from "./logUtility.js"
 
-export function loggerRunner(logLevel, env, compiledLogPattern, logFormat, log) {
-    const chalkLog = ChalkLog[logLevel] || ChalkLog.CUSTOM
-    if (logFormat == LogFormats.JSON) {
-        prepareJSONLog(logLevel, log, env, function (LogJSON) {
-            consoleLogJSON(logLevel, chalkLog, LogJSON)
-        })
-    } else if (logFormat == LogFormats.STATEMENT) {
-        prepareStatementLog(logLevel, log, env, compiledLogPattern, function (LogStatement) {
-            consoleLogStatement(logLevel, chalkLog, LogStatement)
-        })
-    }
+export function JSONLogDriver(log, loglevel, logenv, callback) {
+    const preparedJSONLog = prepareJSONLog(loglevel, log, logenv)
+    
+    callback(preparedJSONLog)
 }
