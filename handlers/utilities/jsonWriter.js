3e29d664d925751b6a89a20cbbcfedb7a0b995e0
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-06-17 19:14:56 +0530

    Revert "Using writeFileSync inplace of writeFile in JSONWriterGeneric"
    This reverts commit 02d526a975195748f3483688c66be40592944866.

--- handlers/utilities/jsonWriter.js
+++ handlers/utilities/jsonWriter.js
@@ -2,6 +2,11 @@ import * as filesystem from 'fs';
 
 export function JSONWriterGeneric(filePath, contentToWrite, callback) {
     const jsonifiedContentToWrite = JSON.stringify(contentToWrite, null, 4);
-    filesystem.writeFileSync(filePath, jsonifiedContentToWrite)
-    callback()
+    filesystem.writeFile(filePath, jsonifiedContentToWrite, function (err) {
+        if (err) {
+            callback(err);
+        }else{
+            callback();
+        }
+    })
 }
